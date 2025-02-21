fc09cab8caae7272ab7fb2eac2c9f44068aad151
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2025-02-21 17:20:04 +0000

    wip struct eta axiom generation

--- tests/lean/dfEq.lean
+++ tests/lean/dfEq.lean
@@ -1,8 +1,14 @@
 import Lean.Meta.Tactic.DfEq
 
-axiom S : (A : Type) → (B : A → Type) → Type
-axiom mkS : (A : Type) → (B : A → Type) → (a : A) → B a → S A B
+structure S where
+x : Nat
+y : Nat
+z : Nat
 
+def S.structEtaAux (s : S) : s = S.mk s.x s.y s.z :=
+  @s.rec (fun s' => s' = S.mk s'.x s'.y s'.z) (fun x y z => Eq.refl (S.mk x y z))
+
+def S.structEta (s s' : S) (hx : s.x = s'.x) (hy : s.y = s'.y) (hz : s.z = s'.z) : s = S.mk s'.x s'.y s'.z := sorry
 
 -- @[dfeq]
 -- axiom add_1 (n : Nat) : 1 + n = .succ n
